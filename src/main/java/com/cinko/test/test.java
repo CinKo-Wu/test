d46439d85771cdc48a03d67a5d6e16715493d3be
cinko <test@cinko.cn>
2020-10-09 22:35:11 +0800

    创建User类，并将test移入test目录

---
+++ src/main/java/com/cinko/test/test.java
@@ -0,0 +1,11 @@
+package com.cinko.test;
+
+import com.cinko.domain.User;
+
+public class test {
+    public static void main(String[] args) {
+        User user = new User(1,"zhangsan",25);
+        System.out.println("Hello World");
+        System.out.println(user);
+    }
+}
